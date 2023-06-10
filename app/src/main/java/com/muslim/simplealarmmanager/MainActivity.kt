5a11018d2d4b2d0570d7740ce6a61be2f854e0e9
Murad <engno_95@mail.ru>
2023-06-10 12:48:00 +0300

    start commit

---
+++ app/src/main/java/com/muslim/simplealarmmanager/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.muslim.simplealarmmanager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.muslim.simplealarmmanager.databinding.ContentMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ContentMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ContentMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
